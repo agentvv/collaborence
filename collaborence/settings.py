a937055fa34763e297763899facee924eab4d75e
Lewis Able Rafuse <rafusel@mcmaster.ca>
2020-01-25 18:42:01 -0500

    Installed apps in project.

--- collaborence/settings.py
+++ collaborence/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'schools.app.SchoolsConfig',
+    'courses.app.CoursesConfig',
+    'slides.app.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
