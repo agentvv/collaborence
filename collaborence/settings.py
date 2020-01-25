d8ae45fb75c3d5417e4dbbada1892d68967f9d3d
Victor Vezina <vezinav@mcmaster.ca>
2020-01-25 17:30:22 -0500

    Modified settings.py, added home app, added .gitignore and README.md

--- collaborence/settings.py
+++ collaborence/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'collaborence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'collaborence',
+        'USER': 'postgres',
+        'PASSWORD': 'ubuntu',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
