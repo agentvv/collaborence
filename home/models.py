6a74ca76ff5c6eaeeaed630bdb1a16ba41409ced
Andrew Hum <huma3@mcmaster.ca>
2020-01-25 18:23:24 -0500

    Update models.py

--- home/models.py
+++ home/models.py
@@ -9,3 +9,14 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class School(models.Model):
+    school_name = models.CharField(max_length=50)
+    school_location = models.CharField(max_lengths=6, help_text='Example: ON, CA') # ON, CA
+
+class Courses(models.Model):
+    school_name = models.ForeignKey(School, on_delete=models.CASCADE)
+    course_name = models.CharField(max_lengths=50)
+    course_department = models.CharField(max_lengths=50)
+    course_term = models.CharField(max_lengths=10)
+    course_id = models.CharField(max_lengths=10)
