397b039d3633a8c91f1068ca4b5a057e24eb8188
Lewis Able Rafuse <rafusel@mcmaster.ca>
2020-01-25 19:27:24 -0500

    Deleted models from home.

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,3 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
-
-admin.site.register(Question)
-admin.site.register(School)
-admin.site.register(Course)
--- home/models.py
+++ home/models.py
@@ -1,16 +1,1 @@
 from django.db import models
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-class School(models.Model):
-    school_name = models.CharField(max_length=50)
-    school_location = models.CharField(max_length=6, help_text='Example: ON, CA') # ON, CA
-
-class Course(models.Model):
-    school_name = models.ForeignKey(School, on_delete=models.CASCADE)
-    course_name = models.CharField(max_length=50)
-    course_department = models.CharField(max_length=50)
-    course_term = models.CharField(max_length=10)
-    course_id = models.CharField(max_length=10)
