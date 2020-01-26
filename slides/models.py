0e1e59094b9cba88f31a08d402fa6bd790edb68a
Andrew Hum <huma3@mcmaster.ca>
2020-01-25 20:25:10 -0500

    Created slides model

--- slides/models.py
+++ slides/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Slide(models.Model):
+    title = models.CharField(max_length=30)
+    fileName = models.CharField(max_length=50)
+    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
+    
+    def __str__ (): 
+        return self.name
