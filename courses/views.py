8fbec70a81c026a11a7c4aac9a4a60847f245c29
Lewis Able Rafuse <rafusel@mcmaster.ca>
2020-01-26 03:00:30 -0500

    Added schools view.

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import FileResponse, Http404, HttpResponse
 
-from .models import Course
+from .models import Course, School
 
 def index(response):
     #course = course.objects.filter(department=)
@@ -10,3 +10,7 @@ def index(response):
 def view(response, num):
 	#bla
 	return render(response, 'slides/view.html', {"fileName":fileName});
+
+def schools(response):
+    schools = School.objects.all()
+    return render(response, 'courses/schools.html', {"schools":schools})
