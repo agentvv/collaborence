244179faf2f53ccab9a52becd209ca5ec14d2058
Andrew Hum <huma3@mcmaster.ca>
2020-01-25 19:05:07 -0500

    Update models.py

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    school_name = models.CharField(max_length=50)
+    school_location = models.CharField(max_length=6, help_text='Example: ON, CA') # ON, CA
+    
+class Courses(models.Model):
+    school_name = models.ForeignKey(School, on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=50)
+    course_department = models.CharField(max_length=50)
+    course_term = models.CharField(max_length=10)
+    course_id = models.CharField(max_length=10)
