78795272c4dc78b653a34d88afc7c20d920ab701
Victor Vezina <vezinav@mcmaster.ca>
2020-01-25 21:50:09 -0500

    Modified models in courses app

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,17 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+def getCurrentTerm():
+	month = timezone.now().month
+	if month >= 9:
+		return 'Fall'
+	elif month >= 7:
+		return 'Summer'
+	elif month >= 5:
+		return 'Spring'
+	else:
+		return 'Winter'
+
 class School(models.Model):
 	name = models.CharField(max_length=100)
 	location = models.CharField(max_length=100)
@@ -23,9 +34,11 @@ class Course(models.Model):
 	code = models.CharField(max_length=10)
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=250)
+	year = models.IntegerField(default=timezone.now().year)
+	term = models.CharField(max_length=10, default=getCurrentTerm())
 
 	def __str__(self):
-		return self.department.shortForm + ' ' + self.code + ': ' + self.name
+		return self.department.shortForm + ' ' + self.code + ': ' + self.name + ' ' + self.term + ' ' + str(self.year)
 
 	def getFullCode(self):
 		return self.department.shortForm + ' ' + self.code
