61c9368dfff1a1dfa898eca0d92181cc8f921749
Victor Vezina <vezinav@mcmaster.ca>
2020-01-26 02:40:12 -0500

    Adding changes so Lewis can pull.

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import FileResponse, Http404, HttpResponse
 
-# Create your views here.
+from .models import Course
+
+def index(response):
+    #course = course.objects.filter(department=)
+    return render(response, 'course/course.html')
+
+def view(response, num):
+	#bla
+	return render(response, 'slides/view.html', {"fileName":fileName});
